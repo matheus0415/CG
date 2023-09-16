a3135c628b4c4fe693248f577031e84b6b7f2b99
Matheus Santos <somathsant@gmail.com>
2023-09-16 14:16:21 -0300

    first commit

---
+++ meucanvas.h
@@ -0,0 +1,15 @@
+#ifndef MEUCANVAS_H
+#define MEUCANVAS_H
+
+#include <QOpenGLWidget>
+
+class MeuCanvas : public QOpenGLWidget
+{
+public:
+    MeuCanvas(QWidget *parent);
+
+    void initializeGL();
+    void paintGL();
+};
+
+#endif // MEUCANVAS_H
